08eeef872673f8338d586ffde328f980b8592662
克格勃佳佳 <jiasansui1573@gmail.com>
2021-03-13 10:44:43 +0800

    spring-boot-starter-rabbitmq

---
+++ src/main/java/com/lwj/seckill/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.lwj.seckill.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabbitMQ配置类
+ */
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public Queue queue(){
+        return new Queue("queue",true);
+    }
+}
--- src/main/java/com/lwj/seckill/controller/UserController.java
+++ src/main/java/com/lwj/seckill/controller/UserController.java
@@ -2,7 +2,9 @@ package com.lwj.seckill.controller;
 
 
 import com.lwj.seckill.pojo.User;
+import com.lwj.seckill.rabbitmq.MQSender;
 import com.lwj.seckill.vo.RespBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @RequestMapping("/test")
-    public String test(){
-        return "hello";
-    }
-    @RequestMapping("info")
+
+    @Autowired
+    private MQSender mqSender;
+
+
+    @RequestMapping("/info")
     @ResponseBody
     public RespBean info(User user){
         return RespBean.success(user);
+    }
+
+    /**
+     * 测试rabbitmq
+     */
+    @RequestMapping("/mq")
+    @ResponseBody
+    public void mq(){
+       mqSender.send("hello lwj");
     }
 }
