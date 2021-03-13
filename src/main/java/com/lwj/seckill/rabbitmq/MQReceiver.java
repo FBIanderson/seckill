fec69a51a9630831d324e87e47be39f5b76908bf
克格勃佳佳 <jiasansui1573@gmail.com>
2021-03-13 18:34:00 +0800

    rabbitmq config

--- src/main/java/com/lwj/seckill/rabbitmq/MQReceiver.java
+++ src/main/java/com/lwj/seckill/rabbitmq/MQReceiver.java
@@ -1,6 +1,7 @@
 package com.lwj.seckill.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +14,40 @@ public class MQReceiver {
     @RabbitListener(queues = "queue")
     public void receive(Object msg){
         log.info("接收消息"+msg);
+    }
+    @RabbitListener(queues = "queue_fanout01")
+    public void receive01(Object msg){
+        log.info("QUEUE01接收消息"+msg);
+    }
+    @RabbitListener(queues = "queue_fanout02")
+    public void receive02(Object msg){
+        log.info("QUEUE02接收消息"+msg);
+    }
+    @RabbitListener(queues = "queue_direct01")
+    public void receive03(Object msg){
+        log.info("QUEUE01接收消息"+msg);
+    }
+    @RabbitListener(queues = "queue_direct02")
+    public void receive04(Object msg){
+        log.info("QUEUE02接收消息"+msg);
+    }
+    @RabbitListener(queues = "queue_topic01")
+    public void receive05(Object msg){
+        log.info("QUEUEU01接收消息"+msg);
+    }
+    @RabbitListener(queues = "queue_topic02")
+    public void receive06(Object msg){
+        log.info("QUEUEU02收消息"+msg);
+    }
+
+    @RabbitListener(queues = "queue_header01")
+    public void receive07(Message msg){
+        log.info("QUEUE01接收对象"+msg);
+        log.info("QUEUE01接收消息"+new String(msg.getBody()));
+    }
+    @RabbitListener(queues = "queue_header02")
+    public void receive08(Message msg){
+        log.info("QUEUE02接收对象"+msg);
+        log.info("QUEUE02接受消息"+new String(msg.getBody()));
     }
 }
--- src/main/java/com/lwj/seckill/rabbitmq/MQSender.java
+++ src/main/java/com/lwj/seckill/rabbitmq/MQSender.java
@@ -1,6 +1,8 @@
 package com.lwj.seckill.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,43 @@ public class MQSender {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
     public void send(Object msg){
           log.info("发送消息"+msg);
-          rabbitTemplate.convertAndSend("queue",msg);
+          rabbitTemplate.convertAndSend("fanoutExchange","",msg);
     }
+    public void sendDirect01(Object msg){
+        log.info("发送red消息"+msg);
+        rabbitTemplate.convertAndSend("directExchange","queue.red",msg);
+    }
+    public void sendDirect02(Object msg){
+        log.info("发送green消息"+msg);
+        rabbitTemplate.convertAndSend("directExchange","queue.green",msg);
+    }
+
+    public void sendTopic01(Object msg){
+        log.info("发送消息（QUEUE01接收)"+msg);
+        rabbitTemplate.convertAndSend("topicExchange","queue.red.message",msg);
+    }
+    public void sendTopic02(Object msg){
+       log.info("发送消息(被两个queue接受)"+msg);
+       rabbitTemplate.convertAndSend("topicExchange","message.queue.green.abc",msg);
+    }
+    public void sendHeaders01(String msg){
+        log.info("发送消息（被两个消息接收）",msg);
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("color","red");
+        messageProperties.setHeader("speed","fast");
+        Message message = new Message(msg.getBytes(),messageProperties);
+        rabbitTemplate.convertAndSend("headersExchange","",message);
+    }
+    public void sendHeaders02(String msg){
+        log.info("发送消息(被QUEUE01接收)",msg);
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("color","red");
+        messageProperties.setHeader("speed","normal");
+        Message message = new Message(msg.getBytes(),messageProperties);
+        rabbitTemplate.convertAndSend("headersExchange","",message);
+    }
+
 }
